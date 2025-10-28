4d0c31c28961072b511c8bc9fa33f9044e13f1da
Maxamed459 <maxamedmahdi459@gmail.com>
2025-10-28 21:16:51 +0300

    add login method

--- state/authentication/autheSlice.ts
+++ state/authentication/autheSlice.ts
@@ -2,7 +2,7 @@ import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { UserRegistration, User, authState } from "@/types/types";
 import axios, { AxiosError } from "axios";
 import { AUTH_BASE_URL } from "../baseURL";
-import { registerFormData } from "@/schema/schema";
+import { loginFormData, registerFormData } from "@/schema/schema";
 
 const initialState: authState = {
   user: null,
@@ -17,6 +17,25 @@ export const register = createAsyncThunk(
     try {
       const payload = { ...userData, confirmation: userData.confirm };
       const res = await axios.post(`${AUTH_BASE_URL}/register`, payload, {
+        headers: { "Content-Type": "application/json" },
+      });
+      return res.data;
+    } catch (error) {
+      const axiosError = error as AxiosError<{ message: string }>;
+      const message =
+        axiosError.response?.data?.message ||
+        axiosError.message ||
+        "Request failed";
+      return rejectWithValue(message);
+    }
+  }
+);
+
+export const login = createAsyncThunk(
+  "auth/login",
+  async (userData: loginFormData, { rejectWithValue }) => {
+    try {
+      const res = await axios.post(`${AUTH_BASE_URL}/login`, userData, {
         headers: { "Content-Type": "application/json" },
       });
       return res.data;
@@ -52,10 +71,28 @@ const authSlice = createSlice({
         (state, action: PayloadAction<UserRegistration>) => {
           state.loading = false;
           state.error = null;
-          if (action.payload) state.user = action.payload;
+          if (action.payload) state.user = action.payload.user;
+          if (action.payload) state.token = action.payload.token;
+          localStorage.setItem("token", JSON.parse(action.payload.token));
         }
       )
       .addCase(register.rejected, (state, action) => {
+        (state.loading = false), (state.error = action.payload as string);
+      })
+      .addCase(login.pending, (state) => {
+        (state.loading = true), (state.error = null);
+      })
+      .addCase(
+        login.fulfilled,
+        (state, action: PayloadAction<UserRegistration>) => {
+          state.loading = false;
+          state.error = null;
+          if (action.payload) state.user = action.payload.user;
+          if (action.payload) state.token = action.payload.token;
+          localStorage.setItem("token", JSON.parse(action.payload.token));
+        }
+      )
+      .addCase(login.rejected, (state, action) => {
         (state.loading = false), (state.error = action.payload as string);
       });
   },
