b86cb4eb3d93b98c0cbde99c5b090a5b39cbfc83
Maxamed459 <maxamedmahdi459@gmail.com>
2025-11-09 22:13:02 +0300

    add preloading the user and token from the localstorage

--- state/store.ts
+++ state/store.ts
@@ -2,9 +2,26 @@ import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "./authentication/authSlice";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
+const preloadedUser =
+  typeof window !== "undefined"
+    ? JSON.parse(localStorage.getItem("user") || "null")
+    : null;
+const preloadedToken =
+    typeof window !== "undefined"
+      ? localStorage.getItem("token") || "null"
+      : null;
+
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+  },
+  preloadedState: {
+    auth: {
+      user: preloadedUser,
+      token: preloadedToken,
+      loading: false,
+      error: null,
+    },
   },
 });
 
