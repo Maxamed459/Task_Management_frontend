a2d4a1480f144420921a2ec75586205277ca7198
Maxamed459 <maxamedmahdi459@gmail.com>
2025-11-17 18:53:48 +0300

    updated the ui

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,17 @@
+"use client"
+import { useAppSelector } from "@/state/store";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Dashboard() {
+  const {user} = useAppSelector((state) => state.auth)
+  const router = useRouter()
+
+  useEffect(() => {
+      if (!user) {
+        router.push("/auth/login");
+      }
+    }, [user, router]); 
   return (
     <div className="w-full max-w-[90%] flex items-center justify-center h-screen">
       <div>
