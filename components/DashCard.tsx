cd0051d6d25c1c945e7e13a1a594eee9616fcb39
Maxamed459 <maxamedmahdi459@gmail.com>
2025-11-24 17:21:32 +0300

    added dashboard card

---
+++ components/DashCard.tsx
@@ -0,0 +1,17 @@
+import { ListChecks } from "lucide-react";
+
+export default function DashCard({icon, title, text, subtext}: 
+    {icon: React.ReactNode, title: string, text: string, subtext: string}) {
+    return(
+        <div className="p-4 border border-black/10 shadow-md flex items-center justify-between w-72 rounded-md">
+            <div className="">
+                <p className="text-sm text-gray-600">{title}</p>
+                <h4 className="text-2xl font-semibold">{text}</h4>
+                <p className="text-sm text-gray-600">{subtext}</p>
+            </div>
+            <div className="w-12 h-12 flex items-center justify-center rounded-lg bg-blue-100 ">
+                {icon}
+            </div>
+        </div>
+    );
+}
