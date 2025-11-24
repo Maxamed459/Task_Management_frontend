a5d8dfed84dcd6afaa2e4c99a9e42c733db0bf60
Maxamed459 <maxamedmahdi459@gmail.com>
2025-11-24 17:21:49 +0300

    add sidebar

---
+++ components/Sidebar.tsx
@@ -0,0 +1,118 @@
+"use client";
+import { useAppSelector } from "@/state/store";
+import { ChevronFirst, ChevronLast, LogOutIcon } from "lucide-react";
+import Image from "next/image";
+import { Children, createContext, useContext, useState } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+
+interface SidebarContextType {
+  expanded: boolean;
+}
+
+const SidebarContext = createContext<SidebarContextType>({
+    expanded: true
+})
+export function Sidebar({children}: {children: React.ReactNode}) {
+    const {user} = useAppSelector((state) => state.auth)
+    const [ expanded, setExpanded ] = useState(true)
+    return(
+        <aside className="h-screen">
+            <nav className="h-full flex flex-col bg-white border-r shadow-sm">
+                <div className="p-4 pb-2 flex items-center justify-between">
+                    <Image src="/task_logo.png" width={120} height={120} alt="Tasky logo" className={`overflow-hidden transition-all ${
+                        expanded ? "w-32" : "w-0"
+                    }`} />
+                    <button className="p-1.5 rounded-lg bg-gray-100" onClick={() => setExpanded((curr) => !curr)}>
+                        {
+                            expanded ? <ChevronFirst /> : <ChevronLast />
+                        }
+                    </button>
+                </div>
+                <SidebarContext.Provider value={{expanded}}>
+                    <ul className="flex-1 px-3">{children}</ul>
+                </SidebarContext.Provider>
+                <div className="border-t flex items-center p-3">
+                    <div className="w-10 h-10 flex items-center justify-center rounded-full bg-linear-to-r from-purple-600 to-blue-600 text-center">
+                        <p className="text-center text-xl text-white font-medium">{user?.full_name.charAt(0).toUpperCase()}</p>
+                    </div>
+                    <div className={`flex justify-between items-center overflow-hidden transition-all ${
+                        expanded ? "w-55 mx-3" : "w-0"
+                    }`}>
+                        <div className="leading-5">
+                            <h4 className=" font-semibold">{user?.full_name}</h4>
+                            <span className="text-xs">{user?.email}</span>
+                        </div>
+                    <LogOutIcon className="text-black" size={20} />
+                    </div>
+                </div>
+            </nav>
+        </aside>
+    );
+}
+
+
+export function SidebarItem({
+    icon,
+    text,
+    href,
+}: {
+    icon: React.ReactNode;
+    text: string;
+    href: string;
+}) {
+    const { expanded } = useContext(SidebarContext);
+    const pathname = usePathname();
+
+    const isActive = pathname === href;
+
+    return (
+        <Link href={href}>
+            <li
+                className={`
+                relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors
+                ${
+                    isActive
+                        ? "bg-linear-to-tr from-blue-600 to-purple-600 text-white"
+                        : "hover:bg-blue-100 text-gray-600"
+                }
+            `}
+            >
+                {icon}
+                <span
+                    className={`overflow-hidden transition-all ${
+                        expanded ? "w-52 ml-3" : "w-0"
+                    }`}
+                >
+                    {text}
+                </span>
+            </li>
+        </Link>
+    );
+}
+
+
+// export function SidebarItem({icon, text, href}: {
+//     icon: React.ReactNode;
+//     text: string;
+//     active?: boolean;
+//     alert?: boolean;
+// }) {
+//     const {expanded} = useContext(SidebarContext)
+//     const [ active, setActive ] = useState(false)
+//     return(
+//         <li className={`relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors 
+//             ${
+//                 active
+//                 ? "bg-linear-to-tr from-blue-200 to-blue-100 text-blue-800"
+//                 : "hover:bg-purple-50 text-gray-600"
+//             }
+//         `} onClick={() => setActive((curr) => !curr)}>
+//             {icon}
+//             <span className={`overflow-hidden transition-all ${
+//                 expanded ? "w-52 ml-3" : "w-0"
+//             }`}>{text}</span>
+//         </li>
+//     );
+// }
