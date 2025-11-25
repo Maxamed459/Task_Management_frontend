99b0f673209f7f0643c62c4ffbec9e21263b02cf
Maxamed459 <maxamedmahdi459@gmail.com>
2025-11-25 17:13:56 +0300

    add handleChange function

--- app/dashboard/add-tasks/page.tsx
+++ app/dashboard/add-tasks/page.tsx
@@ -1,4 +1,19 @@
+import { useState } from "react";
+
 export default function AddTasksPage() {
+
+    const [taskData, setTAskData] = useState({
+        title: "",
+        description: "",
+        due_date: "",
+        priority: "",
+        is_completed: "",
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+
+    }
+
     return(
         <div className="p-4 mt-8 flex items-center justify-center h-sreen w-full">
             <div className="bg-white border border-black/10 shadow-md p-4 w-full max-w-[95%] mx-auto">
@@ -9,19 +24,19 @@ export default function AddTasksPage() {
                 <form className="flex flex-col space-y-5">
                     <div className="grid gap-2">
                         <label htmlFor="title">Title</label>
-                        <input type="text" id="title" placeholder="Enter the task title" name="title" className="border border-blue-600/60 px-4 py-2 rounded-md outline-0 focus:outline-1 focus:outline-blue-600 focus:shadow-md focus:shadow-blue-600/20 w-full" />
+                        <input type="text" id="title" value={taskData.title} onChange={handleChange} placeholder="Enter the task title" name="title" className="border border-blue-600/60 px-4 py-2 rounded-md outline-0 focus:outline-1 focus:outline-blue-600 focus:shadow-md focus:shadow-blue-600/20 w-full" />
                     </div>
                     <div className="grid gap-2">
                         <label htmlFor="description">Description</label>
-                        <input type="text" id="description" placeholder="Enter the task description" name="description" className="border border-blue-600/60 px-4 py-2 rounded-md outline-0 focus:outline-1 focus:outline-blue-600 focus:shadow-md focus:shadow-blue-600/20 w-full" />
+                        <input type="text" value={taskData.title} onChange={handleChange} id="description" placeholder="Enter the task description" name="description" className="border border-blue-600/60 px-4 py-2 rounded-md outline-0 focus:outline-1 focus:outline-blue-600 focus:shadow-md focus:shadow-blue-600/20 w-full" />
                     </div>
                     <div className="grid gap-2">
                         <label htmlFor="due_date">Due date</label>
-                        <input type="date" id="due_date" placeholder="Enter the task due_date" name="due_date" className="border border-blue-600/60 px-4 py-2 rounded-md outline-0 focus:outline-1 focus:outline-blue-600 focus:shadow-md focus:shadow-blue-600/20 w-full" />
+                        <input type="date" value={taskData.title} onChange={handleChange} id="due_date" placeholder="Enter the task due_date" name="due_date" className="border border-blue-600/60 px-4 py-2 rounded-md outline-0 focus:outline-1 focus:outline-blue-600 focus:shadow-md focus:shadow-blue-600/20 w-full" />
                     </div>
                     <div className="grid gap-2">
                         <label htmlFor="priority">Priority</label>
-                        <select className="border border-blue-600/60 px-4 py-2 rounded-md outline-0 focus:outline-1 focus:outline-blue-600 focus:shadow-md focus:shadow-blue-600/20 w-full">
+                        <select on className="border border-blue-600/60 px-4 py-2 rounded-md outline-0 focus:outline-1 focus:outline-blue-600 focus:shadow-md focus:shadow-blue-600/20 w-full">
                             <option defaultChecked disabled >Choose the task priority</option>
                             <option value="low">Low</option>
                             <option value="medium">Medium</option>
