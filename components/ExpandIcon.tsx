23f3ac5fe867374599b2cc35995b8c9c7dd26267
Maxamed459 <maxamedmahdi459@gmail.com>
2025-11-25 12:17:07 +0300

    add panel icon for opening and closing the sidebar

---
+++ components/ExpandIcon.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { ChevronFirst, ChevronLast, PanelLeft } from "lucide-react";
+import { useSidebar } from "@/components/SidebarProvider";
+
+export default function ExpandIcon() {
+  const { expanded, toggle } = useSidebar();
+
+  return (
+    <button
+      className={`p-1.5 rounded-lg bg-gray-100 absolute m-4 ${
+        expanded ? 'left-[290px] mt-2' : 'left-0 mt-2'
+      }`}
+      onClick={toggle}
+    >
+      {expanded ? <PanelLeft className="w-5 h-5" /> : <PanelLeft className="w-5 h-5" />}
+    </button>
+  );
+}
+
+
