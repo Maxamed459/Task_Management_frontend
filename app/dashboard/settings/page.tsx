488cb760ac0e4504d830904ae5e72436934d1ed9
Maxamed459 <maxamedmahdi459@gmail.com>
2025-11-28 17:09:44 +0300

    add content

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -1,7 +1,13 @@
+import { Settings } from "lucide-react";
+
 export default function SettingsPage() {
     return(
-        <div className="p-4">
-            <h1>This is the settings page</h1>
+        <div className="w-full flex items-center justify-center h-screen">
+            <div className="space-y-3 text-center">
+                <h1 className="text-2xl font-semibold md:text-5xl">Welcome to the setting...</h1>
+                <p className="text-sm md:text-xl text-gray-600">This feature is not available now we will add ASAB thanks for your patience</p>
+                <Settings size={150} className="text-center mx-auto mt-15 animate-pulse" />
+            </div>
         </div>
     );
 }
