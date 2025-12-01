3170c324c6a81ac98dfcae9fe9455e237319e930
Maxamed459 <maxamedmahdi459@gmail.com>
2025-12-01 17:03:25 +0300

    updated the ui

--- components/DashCard.tsx
+++ components/DashCard.tsx
@@ -1,8 +1,8 @@
 import { ListChecks } from "lucide-react";
 
-export default function DashCard({icon, title, text, subtext}: 
-    {icon: React.ReactNode, title: string, text: string, subtext: string}) {
-    return(
+export default function DashCard({ icon, title, text, subtext }:
+    { icon: React.ReactNode, title: string, text: string, subtext: string }) {
+    return (
         <div className="p-4 border border-black/10 shadow-sm flex items-center justify-between w-full rounded-md">
             <div className="">
                 <p className="text-sm text-gray-600">{title}</p>
