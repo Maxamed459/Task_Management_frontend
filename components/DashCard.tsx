8fe0f306db1219a19f80d33cafaf8bcba1785358
Maxamed459 <maxamedmahdi459@gmail.com>
2025-12-04 21:51:11 +0300

    updated the ui

--- components/DashCard.tsx
+++ components/DashCard.tsx
@@ -1,9 +1,12 @@
-import { ListChecks } from "lucide-react";
+import { useRouter } from "next/navigation";
 
-export default function DashCard({icon, title, text, subtext}: 
-    {icon: React.ReactNode, title: string, text: string, subtext: string}) {
+export default function DashCard({icon, title, text, subtext, location}: 
+    {icon: React.ReactNode, title: string, text: string, subtext: string, location: string}) {
+        const router = useRouter()
     return(
-        <div className="p-2 border border-black/10 shadow-sm flex items-center justify-between w-full rounded-md">
+        <div className="p-2 border border-black/10 shadow-sm flex items-center justify-between w-full rounded-md cursor-pointer hover:scale-101 transition-shadow duration-300 hover:shadow-lg" onClick={() => {
+            router.push(`${location}`)
+        }}>
             <div className="">
                 <p className="text-sm text-gray-600">{title}</p>
                 <h4 className="text-2xl font-semibold">{text}</h4>
